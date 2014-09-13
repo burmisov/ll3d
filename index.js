587f030be01b4fb0f371732c6844c31ccb75e0dc
Sergey Burmisov <burmisov@informatica.ru>
2014-09-13 11:50:34 +0400

    initial

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var browserify = require('browserify-middleware');
+var path = require('path');
+
+var app = express();
+app.set('view engine', 'jade');
+
+app.use('/data', express.static(path.join(process.cwd(), 'data'), {
+	setHeaders: function (res) {
+		res.header('Content-type', 'application/json; charset=utf-8');
+	}
+}));
+
+app.get('/', function (req, res) {
+	res.render('index');
+});
+
+var port = process.env.PORT || 4080;
+app.listen(port);
+console.log('Server listening on port', port);
